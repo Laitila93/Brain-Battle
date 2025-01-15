9acae7aa5797e7697e542c64d3f3942632e9240a
Laitila93 <Laitila93@gmail.com>
2025-01-15 22:56:31 +0100

    sfg

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'ads.txt',
+          dest: ''
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
